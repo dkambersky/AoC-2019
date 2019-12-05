3d65c6afe7ef947aa811d3fde7a36291c7f29acf
David Kambersky <david.kambersky@gmail.com>
2019-12-05 18:36:12 +0000

    Cleanup

--- src/Day5.kt
+++ src/Day5.kt
@@ -1,16 +1,6 @@
 import intcode.Machine
 import java.io.File
 
-
 private val input = File("input-5.txt").readText()
 
-
-fun main() {
-    // The bulk of this day's work is in the Machine class
-    Machine(input).apply {
-        run()
-    }
-
-}
-
-
+fun main() = Machine(input).run()
