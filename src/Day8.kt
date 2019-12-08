e72bfd2bf8c7ef9eb9e307daaebb07e05cfd451a
David Kambersky <david.kambersky@gmail.com>
2019-12-08 18:54:20 +0000

    Day 8 Part 2

--- src/Day8.kt
+++ src/Day8.kt
@@ -3,11 +3,22 @@ import java.io.File
 private val input = File("input-8.txt").readText().toInts()
 fun main() {
     val (width, height) = 25 to 6
+
     val layers = input.chunked(width * height)
     val part1 = layers.minBy { it.count { it == 0 } }?.let {
         it.count { it == 1 } * it.count { it == 2 }
     } ?: -1
     println(part1)
+
+    val part2 = layers.foldRight(layers.last()) { acc, next ->
+        acc.zip(next).map { (new, old) -> if (new == 2) old else new }
+    }
+    println(part2.prettyPrintStr(width))
 }
 
+
 fun String.toInts() = filter { it.isDigit() }.map { it.toString().toInt() }
+fun List<Int>.prettyPrintStr(width: Int) = chunked(width)
+    .joinToString("\n") { it.joinToString("") }
+    .replace('1', '\u2588')
+    .replace('0', ' ')
