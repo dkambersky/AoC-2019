0c7bec3da426ef8a8b4eab2da5b4540c17529955
David Kambersky <david.kambersky@gmail.com>
2019-12-09 14:18:39 +0000

    Day 9 Part 1

--- src/intcode/Machine.kt
+++ src/intcode/Machine.kt
@@ -5,7 +5,6 @@ import intcode.Operation.*
 import kotlinx.coroutines.delay
 import toInts
 import java.lang.IllegalArgumentException
-import java.lang.NumberFormatException
 
 class Machine(
     input: String, val id: Int = -1
@@ -29,23 +28,16 @@ class Machine(
         }
     }
 
-
     private suspend fun step(): Boolean {
         val instruction = Instruction.construct((memory[instPointer] ?: 0L).toInts())
-        val par = instruction.parameters.mapIndexed { i, par -> getValue(par, i) }
-        println("\t Parameters: [${par.joinToString()}]")
+        val par = instruction.parameters.mapIndexed { i, par -> getVal(par, i) }
         val ptr = instPointer
 
         when (instruction.operation) {
             HALT -> return false
             ADD -> setVal(par[2], par[0] + par[1])
             MULTIPLY -> setVal(par[2], par[0] * par[1])
-            INPUT -> {
-                val inp = readInput()
-                println("Writing $inp to ${par[0]}; offset $relativeBase ")
-
-                setVal(par[0], inp)
-            }
+            INPUT -> setVal(par[0], readInput())
             OUTPUT -> outputCallbacks.forEach { it.invoke(par[0]) }
             JUMP_IF_TRUE -> if (par[0] != 0L) instPointer = par[1]
             JUMP_IF_FALSE -> if (par[0] == 0L) instPointer = par[1]
@@ -60,33 +52,33 @@ class Machine(
         return true
     }
 
-    private fun getValue(parameter: Parameter, position: Int): Long {
+    private fun getVal(parameter: Parameter, position: Int): Long {
+        val register = memory[instPointer + position + 1]
 
-        val simpleVal = memory[instPointer + position + 1]
-        println("Parameter $parameter, position $position, simpleVal $simpleVal")
-        return when (parameter.mode) {
-            IMMEDIATE -> simpleVal ?: 0L
-            POSITION -> memory[simpleVal] ?: 0L
-            RELATIVE -> memory[relativeBase + (simpleVal ?: 0L)] ?: 0L
+        if (parameter.write) {
+            return when (parameter.mode) {
+                IMMEDIATE, POSITION -> register ?: 0L
+                RELATIVE -> relativeBase + (register ?: 0L)
+            }
         }
 
-
+        return when (parameter.mode) {
+            IMMEDIATE -> register ?: 0L
+            POSITION -> memory[register] ?: 0L
+            RELATIVE -> memory[relativeBase + (register ?: 0L)] ?: 0L
+        }
     }
 
     private fun setVal(pos: Long, value: Long) {
-//        println("Setting mem at $pos to $value : existing ${memory[pos]}")
         memory[pos] = value
     }
+
 
     fun addInput(input: Long) = inputs.add(input)
     fun registerOutputCallback(callback: (Long) -> Unit) = outputCallbacks.add(callback)
     private suspend fun readInput(): Long {
-        while (inputs.size == inputRead) {
-            delay(10)
-            print(".")
-        }
+        while (inputs.size == inputRead) delay(1)
         return inputs[inputRead++]
-
     }
 
 }
@@ -94,16 +86,13 @@ class Machine(
 class Instruction(val operation: Operation, val parameters: List<Parameter>) {
     companion object {
         fun construct(ints: List<Int>): Instruction {
-            print("Constructing: $ints | ")
             val operation = Operation.byOpcode(ints.takeLast(2).mergeInts())
             val modes = ints.dropLast(2).reversed().toList().toMutableList()
             modes.addAll(generateSequence { 0 }.take(operation.parameters().size - modes.size))
             val parameters = modes.mapIndexed { i, mode ->
                 Parameter(Mode.values()[mode], write = operation.isWrite(i))
             }
-            return Instruction(operation, parameters).apply {
-                println("Constructed operation $this")
-            }
+            return Instruction(operation, parameters)
         }
     }
 
