0992514525024c29ae2d9032319a1ae1d99b15ca
David Kambersky <david.kambersky@gmail.com>
2019-12-10 23:25:25 +0000

    Cleanup

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,27 +1,6 @@
 import java.io.File
-import java.lang.Math.PI
 
 private val input = File("input-10.txt").readText()
-private val input2 = """.#..##.###...#######
-##.############..##.
-.#.######.########.#
-.###.#######.####.#.
-#####.##.#.##.###.##
-..#####..#.#########
-####################
-#.####....###.#.#.##
-##.#################
-#####.##.###..####..
-..######..##.#######
-####.##.####...##..#
-.#####..#.######.###
-##...#.##########...
-#.##########.#######
-.####.#.###.###.#.##
-....##.##.###..#####
-.#.#.###########.###
-#.#.#.#####.####.###
-###.##.####.##.#..##"""
 
 fun main() {
     val asteroids = input.lines().mapIndexed { y, xs ->
@@ -31,58 +10,23 @@ fun main() {
     val station = asteroids.maxBy { visibleFrom(it, asteroids).size }!!
     println(visibleFrom(station, asteroids).size)  // Part 1
 
-    val a = Point(5,5)
-    val b = Point(5, 4)
-    val c = Point(6,5)
-    val d = Point(5,6)
-    val e = Point(4,5)
-    val f = Point(4,4)
-
-
-//    print("EXPECTING 0 ")
-//    println(a.angle(b))
-//
-//    print("EXPECTING 90 ")
-//    println(a.angle(c))
-//
-//    print("EXPECTING 180 ")
-//    println(a.angle(d))
-//
-//    print("EXPECTING 270 ")
-//    println(a.angle(e))
-//
-//    print("EXPECTING 315 ")
-//    println(a.angle(f))
-
-
-
-
     var i = 0
-    while (i <= 199) {
+    outer@ while (true) {
         val visible = visibleFrom(station, asteroids)
-
-        println("Beginning rotation. station $station, first:  ${visible.entries.first()}")
-
-
         for ((_, candidates) in visible.entries) {
-            if(i == 199) {
-                println(candidates.first())
-                i++
-                break
+            if (i == 199) {
+                println(candidates.first().let { it.x * 100 + it.y }) // Part 2
+                break@outer
             }
-            println("Nuking $i -> ${candidates.first()}")
             asteroids.remove(candidates.first())
             i++
         }
     }
-
-
-
 }
 
 fun visibleFrom(asteroid: Point, asteroids: List<Point>) =
     asteroids
         .filterNot { it == asteroid }
-        .groupBy { asteroid.angle(it)  }
+        .groupBy { asteroid.angle(it) }
         .map { it.key to it.value.sortedBy { asteroid.distance(it) } }
         .toMap().toSortedMap()
