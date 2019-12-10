e54072c4f9f246caa5d6af02f7cd332911d15173
David Kambersky <david.kambersky@gmail.com>
2019-12-10 21:59:23 +0000

    Day10 Part1 cleaned up

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,45 +1,22 @@
-import intcode.Machine
 import java.io.File
 
 private val input = File("input-10.txt").readText()
-private val input2 = """.#..#
-.....
-#####
-....#
-...##"""
 
 fun main() {
 
-    val asteroids = mutableListOf<Asteroid>()
-    val map = input.lines().mapIndexed { x, ys ->
-        ys.mapIndexed inner@{ y, c ->
-            if (c == '.') return@inner null
-            Asteroid(x, y).apply {
-                asteroids.add(this)
-                return@inner this
-            }
-        }.filterNotNull()
-    }.flatten()
+    val asteroids = mutableListOf<Point>()
+    input.lines().forEachIndexed { x, ys ->
+        ys.forEachIndexed { y, c ->
+            if (c == '.') asteroids.add(Point(x, y))
+        }
+    }
 
-
-    val a = asteroids.maxBy { visibleFrom(it, asteroids) } ?: throw Exception("WtF")
-    println(a)
-    print(visibleFrom(a, asteroids))
-
+    val best = asteroids.maxBy { visibleFrom(it, asteroids) }
+    print(visibleFrom(best!!, asteroids))
 
 }
 
-fun visibleFrom(asteroid: Asteroid, asteroids: List<Asteroid>): Int =
-    chunked(asteroids, asteroid).sumBy{ it.groupBy { it.slope(asteroid) }.keys.count() }
-
-fun chunked(asteroids: List<Asteroid>, around: Asteroid) =
-    asteroids.groupBy { it.x > around.x }
-        .values.map { it.groupBy { it.y > around.y } }
-        .map { it.values }
-        .flatten()
-
-data class Asteroid(val x: Int, val y: Int) {
-    fun slope(other: Asteroid) =
-    (y - other.y) / (x - other.x)
-        .let { if (it == 0) return Integer.MAX_VALUE - (if (y - other.y < 0) 1 else 2) else it }
-}
+fun visibleFrom(asteroid: Point, asteroids: List<Point>): Int =
+    asteroids
+        .filterNot { it == asteroid }
+        .groupBy { it.angle(asteroid) }.keys.count()
